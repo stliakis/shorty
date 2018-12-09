6bbab45d158082ecd927266f53cf129a43b676fe
Liakis Stefanos <stliakis@gmail.com>
2018-12-09 15:41:13 +0200

    Initial commit

---
+++ shortener.py
@@ -0,0 +1,64 @@
+import base64
+from hashlib import md5
+
+import pymongo
+
+import config
+
+
+class UrlShortener:
+    def __init__(self):
+        mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+        mongo_database = mongo_client["shorty"]
+        self.urls = mongo_database["urls"]
+
+    def shortcode(self, url):
+        """
+        Our main shortening function. The rationale here is that
+        we are relying on the fact that for similarly sized inputs
+        such as URLs the potential for collision in the 32 last bits
+        of the MD5 hash is rather unlikely.
+
+        The following things happen, in order:
+
+        * compute the md5 digest of the given source
+        * extract the lower 4 bytes
+        * base64 encode the result
+        * remove trailing padding if it exists
+
+        Of course, should a collision happen, we will evict the previous
+        key.
+
+        """
+        return base64.b64encode(md5(url.encode('utf-8')).digest()[-8:]).decode("utf-8", "ignore").replace('=',
+                                                                                                          '').replace(
+            '/', '_')
+
+    def shorten(self, url):
+        """
+        The shortening workflow is very minimal. We try to
+        set the redis key to the url value. We catch any
+        exception in the process to properly report failures
+        in the client
+        """
+
+        code = self.shortcode(url)
+
+        if not self.lookup(code):
+            self.urls.insert_one({"hash": code, "url": url})
+
+        return {'success': True,
+                'url': url,
+                'code': code,
+                'shorturl': config.URL_PREFIX + code}
+
+    def lookup(self, code):
+        """
+        The same strategy is used for the lookup than for the
+        shortening. Here a None reply will imply either an
+        error or a wrong code.
+        """
+        entry = self.urls.find_one({"hash": code})
+        if entry:
+            return entry.get("url")
+        return None
---
+++ shorty.py
@@ -0,0 +1,95 @@
+import logging
+from urllib.parse import urlparse
+
+from bernhard import Client
+from flask import Flask, request, redirect, make_response, json
+from riemann_wrapper import riemann_wrapper
+
+import config
+from shortener import UrlShortener
+
+app = Flask(__name__)
+
+shorterer = UrlShortener()
+logger = logging.getLogger()
+
+logger.info("starting up")
+try:
+    riemann_client = Client(host=config.RIEMANN_HOST,
+                            port=config.RIEMANN_PORT)
+    riemann_client.send({'metric': 1,
+                         'service': 'url-shortener.startup',
+                         'ttl': 3600})
+except:
+    riemann_client = None
+
+wrap_riemann = riemann_wrapper(client=riemann_client, prefix='url-shortener.')
+
+
+@app.route('/404')
+@wrap_riemann('missing', tags=['http_404'])
+def missing():
+    response = make_response(json.dumps({
+        "error": "not found"
+    }))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+@app.route('/400')
+@wrap_riemann('invalid', tags=['http_400'])
+def invalid():
+    response = make_response(json.dumps({
+        "error": "invalid"
+    }))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+## short url lookup
+@app.route('/<code>')
+@wrap_riemann('lookup')
+def lookup(code):
+    url = shorterer.lookup(code)
+    if not url:
+        return redirect('/404')
+    else:
+        return redirect(url)
+
+
+@app.route('/g/<code>')
+@wrap_riemann('lookup_json')
+def lookup_json(code):
+    url = shorterer.lookup(code)
+    if not url:
+        return redirect('/404')
+    else:
+        response = make_response(json.dumps({
+            "url": url
+        }))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+## short url generation
+@app.route('/', methods=['POST'])
+@wrap_riemann('creation')
+def shorten_url():
+    if request.json and 'url' in request.json:
+        u = urlparse(request.json['url'])
+        if u.netloc == '':
+            url = 'http://' + request.json['url']
+        else:
+            url = request.json['url']
+        res = shorterer.shorten(url)
+        logger.debug("shortened %s to %s" % (url, res))
+        response = make_response(json.dumps(res))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        logger.info("invalid shorten request")
+        return redirect('/400')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=666)
